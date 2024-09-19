15da660599ece529078e0bbfcd685acdba29e3cb
DanyilMak <danyil.maksymchuk@oa.edu.ua>
2024-09-19 19:59:17 +0300

    Lab2 | add-components

--- Lab2/src/App.jsx
+++ Lab2/src/App.jsx
@@ -1,62 +1,41 @@
 import { useState } from 'react'
 import './App.css'
+import PageTitle from './components/PageTitle'
+import Search from './components/Search'
+import Add from './components/Add'
+import Table from './components/Table'
 
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, title: "delectus aut autem" },
-    { id: 2, title: "quis ut nam facilis et officia qui" },
-  ]);
-  const [newTodo, setNewTodo] = useState("");
-  const [search, setSearch] = useState("");
+    { id: 1, title: 'delectus aut autem' },
+    { id: 2, title: 'quis ut nam facilis et officia qui' },
+  ])
+  const [newTodo, setNewTodo] = useState('')
+  const [search, setSearch] = useState('')
 
   const addTodo = () => {
-    if (newTodo.trim() === "") return;
-    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
-    setTodos([...todos, { id: newId, title: newTodo }]);
-    setNewTodo("");
-  };
+    if (newTodo.trim() === '') return
+    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1
+    setTodos([...todos, { id: newId, title: newTodo }])
+    setNewTodo('')
+  }
 
   const removeTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
 
   const filteredTodos = todos.filter((todo) =>
     todo.title.toLowerCase().includes(search.toLowerCase())
-  );
+  )
 
   return (
-    <div style={{ padding: "20px", minHeight: "100vh" }}>
-      <h1>ToDo App</h1>
-
-      <input
-        type="text"
-        placeholder="Search todos..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        style={{ padding: "8px", fontSize: "1rem", width: "300px", marginRight: "10px", display: "flex", justifyContent: "center"}}
-      />
-
-      <div style={{ marginBottom: "20px", paddingTop: "5px"}}>
-        <input
-          type="text"
-          placeholder="Add new todo..."
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          style={{ padding: "8px", fontSize: "1rem", width: "300px", marginRight: "10px", display: "flex", justifyContent: "center"}}
-        />
-        <button onClick={addTodo} style={{ padding: "10px", fontSize: "0.9rem", cursor: "pointer"}}>Add</button>
-      </div>
-
-      <ul>
-        {filteredTodos.map((todo) => (
-          <li key={todo.id} style={{ padding: "10px 0"}}>
-            {todo.title}{" "}
-            <button onClick={() => removeTodo(todo.id)} style={{ padding: "6px 10px", fontSize: "0.9rem", cursor: "pointer" }}>Remove</button>
-          </li>
-        ))}
-      </ul>
+    <div style={{ padding: '20px', minHeight: '100vh' }}>
+      <PageTitle title="ToDo App" />
+      <Search search={search} setSearch={setSearch} />
+      <Add newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
+      <Table todos={filteredTodos} removeTodo={removeTodo} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
